22234ed92ace8462d7bee9a082be411dd5591851
Jeff Oganezov <joganezov@madisoncollege.edu>
2015-02-07 05:45:18 +0000

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
+
+  def hello
+    render text: "Hello!"
+  end
+  def goodbye
+    render text: "Goodbye!"
+  end
+end
